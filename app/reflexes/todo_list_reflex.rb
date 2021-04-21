7da142b70c17b07de8c5ec49a7e7af0f78b6e471
mgunduz1 <mgunduz114118@gmail.com>
2021-04-21 13:39:21 +0300

    fix deletelist reflex

--- app/reflexes/todo_list_reflex.rb
+++ app/reflexes/todo_list_reflex.rb
@@ -2,7 +2,8 @@ class TodoListReflex < ApplicationReflex
 
     def delete_that_list
       todo_list = TodoList.find(element.dataset.id)
-      todo_list.destroy
+      items = TodoItem.where(todo_list_id: todo_list)
+      items.destroy_all && todo_list.destroy
     end
   
 end
